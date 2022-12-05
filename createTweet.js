b84e9b3836117ae951b34afb983ad97bd916724f
robin2309 <robin.besson1@gmail.com>
2022-12-05 22:22:30 +0100

    create tweets for each user in usersConf json

--- createTweet.js
+++ createTweet.js
@@ -5,21 +5,45 @@ const usersConf = require("./usersConf");
 
 dotenv.config();
 
-console.log(usersConf[1].token);
+const ABSENT = "absent";
 
-const client = new TwitterApi({
-  appKey: process.env.APP_KEY,
-  appSecret: process.env.APP_SECRET,
-  accessToken: usersConf[1].token, // oauth token from previous step (link generation)
-  accessSecret: usersConf[1].secret, // oauth token secret from previous step (link generation)
-});
+const law = "la loi pour protéger les logements de l'occupation illicite";
+const link =
+  "https://www.assemblee-nationale.fr/dyn/16/dossiers/logements_occupationillicite";
 
-client.v2
-  .tweet("test test")
-  .then((val) => {
-    console.log(val);
-    console.log("success");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const getTweet = ({ name, stand, isFemale }) => {
+  const genderAbsent = isFemale ? "ABSENTE" : "ABSENT";
+  const wordedStand =
+    stand === ABSENT
+      ? `était ${genderAbsent} au vote sur`
+      : `a voté ${stand.toUpperCase()}`;
+  return `${name} ${wordedStand} ${law} - ${link}`;
+};
+
+Promise.all(
+  usersConf
+    .filter(({ skip }) => !skip)
+    .map((user) => {
+      const { name, isFemale, stand, token, secret } = user;
+
+      const client = new TwitterApi({
+        appKey: process.env.APP_KEY,
+        appSecret: process.env.APP_SECRET,
+        accessToken: token,
+        accessSecret: secret,
+      });
+
+      const tweet = getTweet({ name, stand, isFemale });
+      console.log(tweet);
+
+      return client.v2
+        .tweet(tweet)
+        .then((val) => {
+          console.log(val);
+          console.log("tweeted for ", name);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+);
